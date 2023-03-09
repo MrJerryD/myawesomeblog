0bf853b09c35fb403d01f464a5f3d52493b1988c
MeJerryD <dkimurik@gmail.com>
2023-03-10 01:07:56 +0200

    6 added new module(app) BLog

--- events/models.py
+++ events/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Events(models.Model):
+class Event(models.Model):
     events_image = models.ImageField(upload_to='event_images')
     events_text = models.CharField(max_length=300)
